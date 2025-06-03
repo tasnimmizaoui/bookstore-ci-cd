6cc4d78e6b5aec4b1cc8bb3bd61e85152afcbe64
tasnimmizaoui <123630827+tasnimmizaoui@users.noreply.github.com>
2025-06-03 21:18:39 +0100

    chnaged the testing properties

--- bookstore/src/test/java/com/example/bookstore/BookstoreApplicationTests.java
+++ bookstore/src/test/java/com/example/bookstore/BookstoreApplicationTests.java
@@ -2,12 +2,15 @@ package com.example.bookstore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // This tells Spring to use application-test.properties
 class BookstoreApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        // This test will now pass because it uses H2 instead of PostgreSQL
+    }
 
 }
